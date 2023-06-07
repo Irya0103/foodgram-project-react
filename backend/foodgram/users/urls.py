59e4a6ff1ea57226845b24583f8ad6a927ba64c7
Ира Андреева <minuto_suerte@mail.ru>
2023-06-07 23:42:23 +0300

    ..

--- backend/foodgram/users/urls.py
+++ backend/foodgram/users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from api.views import CustomUserViewSet
+
 app_name = 'users'
 
 router = DefaultRouter()
+
+router.register('users', CustomUserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
