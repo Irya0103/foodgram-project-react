10d34634810285200ae7fa1ed591c2489d8bea43
Ира Андреева <minuto_suerte@mail.ru>
2023-06-08 13:08:31 +0300

    ..

--- backend/foodgram/users/urls.py
+++ backend/foodgram/users/urls.py
@@ -1,6 +1,7 @@
-from api.views import CustomUserViewSet
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
+from .views import CustomUserViewSet
 
 app_name = 'users'
 
