c69d19ec3940da4c58bcd8fb0eff1b6b32995e64
Ира Андреева <minuto_suerte@mail.ru>
2023-06-09 23:16:16 +0300

    ..

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -47,7 +47,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 
     def get_serializer_class(self):
-        if self.request.method in SAFE_METHODS:
+        if self.request.method == "GET":
             return RecipeReadSerializer
         return RecipeWriteSerializer
 
--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -18,44 +18,40 @@ class CustomUserViewSet(UserViewSet):
     serializer_class = CustomUserSerializer
     pagination_class = PageNumberPaginationLimit
 
-    @action(detail=False,
-            methods=['get'],
-            permission_classes=[IsAuthenticated])
-    def subscriptions(self, request):
+    @action(
+        detail=True,
+        methods=['post', 'delete'],
+        permission_classes=[IsAuthenticated]
+    )
+    def subscribe(self, request, **kwargs):
         user = request.user
-        queryset = User.objects.filter(following__user=user)
-        page = self.paginate_queryset(queryset)
-        serializer = SubscribeSerializer(page,
-                                         many=True,
-                                         context={'request': request})
-        return self.get_paginated_response(serializer.data)
-
-    @action(detail=True,
-            methods=['post', 'delete'],
-            permission_classes=[IsAuthenticated])
-    def subscribe(self, request, id):
-        user = request.user
-        author = get_object_or_404(User, id=id)
+        author_id = self.kwargs.get('id')
+        author = get_object_or_404(User, id=author_id)
 
         if request.method == 'POST':
-            if user.id == author.id:
-                return Response({'detail': 'Нельзя подписаться на себя'},
-                                status=status.HTTP_400_BAD_REQUEST)
-            if Subscribe.objects.filter(author=author, user=user).exists():
-                return Response({'detail': 'Вы уже подписаны!'},
-                                status=status.HTTP_400_BAD_REQUEST)
-            Subscribe.objects.create(user=user, author=author)
             serializer = SubscribeSerializer(author,
-                                             context={'request': request})
+                                             data=request.data,
+                                             context={"request": request})
+            serializer.is_valid(raise_exception=True)
+            Subscribe.objects.create(user=user, author=author)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         if request.method == 'DELETE':
-            if not Subscribe.objects.filter(user=user, author=author).exists():
-                return Response({'errors': 'Вы не подписаны'},
-                                status=status.HTTP_400_BAD_REQUEST)
             subscription = get_object_or_404(Subscribe,
                                              user=user,
                                              author=author)
             subscription.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(
+        detail=False,
+        permission_classes=[IsAuthenticated]
+    )
+    def subscriptions(self, request):
+        user = request.user
+        queryset = User.objects.filter(subscribing__user=user)
+        pages = self.paginate_queryset(queryset)
+        serializer = SubscribeSerializer(pages,
+                                         many=True,
+                                         context={'request': request})
+        return self.get_paginated_response(serializer.data)
