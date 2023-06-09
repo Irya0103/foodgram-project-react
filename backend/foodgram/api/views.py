07c0f4f5f09c736013a678006eb8b8ccc76a1f8e
Ира Андреева <minuto_suerte@mail.ru>
2023-06-09 14:37:08 +0300

    ..

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -52,6 +52,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def favorite(self, request, pk):
         if request.method == 'POST':
             return self.add_to(Favourite, request.user, pk)
+        return self.delete_from(Favourite, request.user, pk)
 
     @action(
         detail=True,
@@ -60,6 +61,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def shopping_cart(self, request, pk):
         if request.method == 'POST':
             return self.add_to(ShoppingCart, request.user, pk)
+        return self.delete_from(ShoppingCart, request.user, pk)
 
     def add_to(self, model, user, pk):
         if model.objects.filter(user=user, recipe__id=pk).exists():
