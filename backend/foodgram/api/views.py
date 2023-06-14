f7ade78c78c50ddbd3b1e5402b1983d7b56ac630
Ира Андреева <minuto_suerte@mail.ru>
2023-06-14 21:31:56 +0300

    ..

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from django.utils import timezone
+from datetime import datetime
 from django.db.models import Sum
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
+from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -87,32 +88,32 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     @action(
         detail=False,
-        permission_classes=[IsAuthenticated])
+        permission_classes=[IsAuthenticated]
+    )
     def download_shopping_cart(self, request):
         user = request.user
         if not user.shopping_cart.exists():
-            return Response(status.HTTP_400_BAD_REQUEST)
+            return Response(status=HTTP_400_BAD_REQUEST)
 
         ingredients = IngredientInRecipe.objects.filter(
             recipe__shopping_cart__user=request.user
         ).values(
             'ingredient__name',
             'ingredient__measurement_unit'
-        ).annotate(addition=Sum('amount'))
+        ).annotate(amount=Sum('amount'))
 
-        time = timezone.now()
-        time = time.strftime("%d-%m-%Y")
+        today = datetime.today()
         shopping_list = (
             f'Список покупок для: {user.get_full_name()}\n\n'
-            f'Дата: {time:%Y-%m-%d}\n\n'
+            f'Дата: {today:%Y-%m-%d}\n\n'
         )
         shopping_list += '\n'.join([
             f'- {ingredient["ingredient__name"]} '
             f'({ingredient["ingredient__measurement_unit"]})'
-            f' - {ingredient["addition"]}'
+            f' - {ingredient["amount"]}'
             for ingredient in ingredients
         ])
-        shopping_list += f'\n\nFoodgram ({time:%Y})'
+        shopping_list += f'\n\nFoodgram ({today:%Y})'
 
         filename = f'{user.username}_shopping_list.txt'
         response = HttpResponse(shopping_list, content_type='text/plain')
