c8c0881f6f45fcc374fe658525dc5c8e92e73d3b
Ира Андреева <minuto_suerte@mail.ru>
2023-06-14 20:43:46 +0300

    ..

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_fremework'
+    'rest_fremework',
     'rest_framework.authtoken',
     'djoser',
     'api',
