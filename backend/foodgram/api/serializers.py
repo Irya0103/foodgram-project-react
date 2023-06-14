1cc463269812d67691f0b61e1eec52a0b328a931
Ира Андреева <minuto_suerte@mail.ru>
2023-06-14 11:48:06 +0300

    ..

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -11,10 +11,8 @@ from rest_framework.relations import PrimaryKeyRelatedField
 from rest_framework.serializers import ModelSerializer
 
 from recipes.models import Ingredient, IngredientInRecipe, Recipe, Tag
-
 from users.models import Subscribe
 
-# зачем нас учили пользоваться "isort .", если он вводит в заблуждение?
 
 User = get_user_model()
 
